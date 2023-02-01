2ac02c10ae558ad79294fd355e1aa7ce42dadc42
alejandro.nieto <alejandro.nieto@madisonmk.com>
2023-02-01 11:48:05 +0100

    Same counter using GetX instead of stock Flutter.

---
+++ lib/home_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+
+  @override
+  Widget build(context) => Scaffold(
+      appBar: AppBar(title: Text("counter")),
+      body: Center(
+        child: Obx(() => Text("${controller.count}")),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: controller.increment,
+      ));
+}
